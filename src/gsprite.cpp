04e5ad57aea7302d26346f6ee304510847df7584
Gian Giovani <wejick@gmail.com>
2012-08-25 09:17:33 +0700

    Udah gak error lagi pas delete Apa bedanya delete atau delete[]

--- src/gsprite.cpp
+++ src/gsprite.cpp
@@ -1,62 +1,67 @@
 #include "gsprite.h"
 
 GSprite::GSprite() {
-        /** Create 3 empty array and add 3 every addition */
-		spritePath = new core::string<fschar_t>[3];
-		clipDimension = new core::rect<u16>[3];
-		itemNumberPerSprite = new int[3];
+        /** Create "ADDER" empty array and add "ADDER" every addition */
+		spritePath = new core::stringc[ADDER];
+		clipDimension = new core::rect<u16>[ADDER];
+		itemNumberPerSprite = new int[ADDER];
 		int count = 0;
-		int arrCount = 3;
+		int arrCount = ADDER;
 }
-void GSprite::addSprite(core::string<fschar_t> sprite,core::rect<u16> clipDim,
+
+void GSprite::addSprite(core::stringc sprite,core::rect<u16> clipDim,
                         int itemNumber) {
 
-        /** If arrCount%adder == 0, we add "adder" empty array again
-        ** So at the worst case, we have "adder-1" unused array
+        /** If arrCount%ADDER == 0, we add "ADDER" empty array again
+        ** So at the worst case, we have "ADDER-1" unused array
         ** But it makes everything more faster
         **/
-        // How much array we add ?
-        int adder = 3;
-        if((arrCount%adder)==0){
+        if((arrCount%ADDER)==0){
             // move the content of (spritePath, clipDimension and itemNumberPerSprite) array to safe place
-            core::string<fschar_t> *spriteTemp = new core::string<fschar_t>[count];
+            core::stringc *spriteTemp = new core::stringc[count];
             core::rect<u16> *clipDimensionTemp = new core::rect<u16>[count];
             int *itemNumberPerSpriteTemp = new int[count];
 
             for(int i=0;i<count;i++){
-                spriteTemp[i]=spritePath[i];
-                clipDimensionTemp[i]=clipDimension[i];
-                itemNumberPerSpriteTemp[i]=itemNumberPerSpriteTemp[i];
+                spriteTemp[i]=this->spritePath[i];
+                clipDimensionTemp[i]=this->clipDimension[i];
+                itemNumberPerSpriteTemp[i]=this->itemNumberPerSprite[i];
             }
+            if(this->spritePath!=NULL)
+                delete[] this->spritePath;
+            if(this->clipDimension!=NULL)
+                delete[] this->clipDimension;
+            if(this->itemNumberPerSprite!=NULL)
+                delete[] this->itemNumberPerSprite;
 
-            delete spritePath;
-            delete clipDimension;
-            delete itemNumberPerSprite;
-
-            // add "adder" empty array
-            spritePath = new core::string<fschar_t>[arrCount+adder];
-            clipDimension = new core::rect<u16>[arrCount+adder];
-            itemNumberPerSprite = new int[arrCount+adder];
+            // add "ADDER" empty array
+            this->spritePath = new core::stringc[arrCount+ADDER];
+            this->clipDimension = new core::rect<u16>[arrCount+ADDER];
+            this->itemNumberPerSprite = new int[arrCount+ADDER];
 
             for(int i=0;i<count;i++){
-                spritePath[i]=spriteTemp[i];
-                clipDimension[i]=clipDimensionTemp[i];
-                itemNumberPerSprite[i]=itemNumberPerSpriteTemp[i];
+                this->spritePath[i]=spriteTemp[i];
+                this->clipDimension[i]=clipDimensionTemp[i];
+                this->itemNumberPerSprite[i]=itemNumberPerSpriteTemp[i];
             }
 
-            delete spriteTemp;
-            delete clipDimensionTemp;
-            delete itemNumberPerSpriteTemp;
+            if(spriteTemp!=NULL)
+                delete[] spriteTemp;
+            if(clipDimensionTemp!=NULL)
+                delete[] clipDimensionTemp;
+            if(itemNumberPerSpriteTemp!=NULL)
+                delete[] itemNumberPerSpriteTemp;
 
-            arrCount=arrCount+adder;
+            this->arrCount=this->arrCount+ADDER;
         }
         // add spritePath to spritePath Array
-        spritePath[count+1] = sprite;
-        clipDimension[count+1] = clipDim;
-        itemNumberPerSprite[count+1] = itemNumber;
+        this->spritePath[count+1] = sprite;
+        this->clipDimension[count+1] = clipDim;
+        this->itemNumberPerSprite[count+1] = itemNumber;
 }
+
 GSprite::~GSprite() {
-    delete spritePath;
-    delete clipDimension;
-    delete itemNumberPerSprite;
+    delete[] spritePath;
+    delete[] clipDimension;
+    delete[] itemNumberPerSprite;
 }
