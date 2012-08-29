2c16f4e01ea2c0b0ca158712e6dada4fe9d94110
Gian Giovani <wejick@gmail.com>
2012-08-29 14:48:16 +0700

    Ganti rect<u16> jadi rect<s32> di clip dimension

--- include/gsprite.h
+++ include/gsprite.h
@@ -11,21 +11,22 @@ class GSprite
 		/** Default constructor */
 		GSprite();
 		/** Add sprite and its specification to Array of spritePath */
-		void addSprite(core::stringc spritePath,core::rect<u16> clipDimension,
+		void addSprite(core::stringc spritePath,core::rect<s32> clipDimension,
 					int itemNumberPerSprite);
 		/** Get sprite path value at any given number (from spritePath array) */
 		core::stringc getSprite(int number) {return spritePath[number];}
+		/** Get clip dimension */
+		core::rect<s32> getClipDimension();
 		int getCount() { return count; }
 		/** Get amount of item value at any given number (from spritePath array) */
 		int getItemNumberPerSprite(int number){return itemNumberPerSprite[number];}
-
 		/** Default destructor */
 		virtual ~GSprite();
 
 	protected:
 	private:
 		core::stringc *spritePath; //!< Array for sprite path
-		core::rect<u16> *clipDimension; //!< Array for clip dimension for sprite's items
+		core::rect<s32> *clipDimension; //!< Array for clip dimension for sprite's items
 		int *itemNumberPerSprite; //!< Number to show how much item per sprite
 		int count; //!< How much sprite you have boy?
 		int arrCount; //!< The real sum of array
